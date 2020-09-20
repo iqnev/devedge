e161fb54fa3c0d4df177708fe9dc7ff05dd26c11
Yanev <y509110@softwareag.com>
2020-09-20 23:26:35 +0300

    fix start/stop functionality of service script.

--- src/main/scrips/service.sh
+++ src/main/scrips/service.sh
@@ -9,93 +9,96 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m' #No Color
+APP="c8y-4-devteams-agent-0.0.1-SNAPSHOT-jar-with-dependencies"
+PGREP=/usr/bin/pgrep
+ZERO=0
 
-# Check the application status
-#
-# This function checks if the application is running
-check_status() {
- check_java
- 
-  # Running ps with some arguments to check if the PID exists
-  # -C : specifies the command name
-  # -o : determines how columns must be displayed
-  # h : hides the data header
-  s=`ps -C 'java -jar c8y-4-devteams-agent-0.0.1-SNAPSHOT-jar-with-dependencies.jar start' -o pid h`
 
-  # If somethig was returned by the ps command, this function returns the PID
-  if [ $s ] ; then
-    return $s
-  fi
-
-  # In any another case, return 0
-  return 0
-
-}
 
 # Starts the application
 start() {
-
-  # At first checks if the application is already started calling the check_status
-  # function
-  pid="$(check_status)"
-
-  if [ "$pid" != 0 ] ; then
-    echo "The application is already started"
-    exit 1
-  fi
-
-  # If the application isn't running, starts it
-  echo -n "Starting application: "
-
-  # Redirects default and error output to a log file
-  nohup java -jar c8y-4-devteams-agent-0.0.1-SNAPSHOT-jar-with-dependencies.jar start >> logfile.log 2>&1 &
-  echo "OK"
+	echo ""		
+    echo "Starting $APP..."
+    check_java
+    #Verify if the service is running
+    $PGREP -f $APP > /dev/null
+    VERIFIER=$?
+    if [ $ZERO -eq $VERIFIER ]
+    then
+        echo -e "The application is ${YELLOW}already${NC} running"
+    else
+        # Redirects default and error output to a log file
+  		nohup java -jar "${APP}.jar" start >> logfile.log 2>&1 &
+        #sleep time before the service verification
+        sleep 3
+        #Verify if the service is running
+        $PGREP -f $APP  > /dev/null
+        VERIFIER=$?
+        if [ $ZERO -eq $VERIFIER ]
+        then
+            echo -e "Apllication was ${GREEN}successfully${NC} started"
+        else
+            echo -e "${RED}Failed${NC} to start application"
+        fi
+    fi
+    echo
 }
 
 # Stops the application
 stop() {
-
-  # Like as the start function, checks the application status
-  pid="$(check_status)"
-
-  pid=$?
-echo -e"PIIII"
-  if [ "$pid" == 0 ] ; then
-    echo "Application is already stopped"
-    exit 1
-  fi
-
-  # Kills the application process
-  echo -n "Stopping application with PID: $pid"
-  kill -9 $pid &
-  echo "OK"
+	echo ""		
+    echo "Stopping $APP..."
+    #Verify if the service is running
+    $PGREP -f $APP > /dev/null
+    VERIFIER=$?
+    if [ $ZERO -eq $VERIFIER ]
+    then        
+		#Kill the pid of java with the application name
+		kill -9 $($PGREP -f $APP)
+		#Sleep time before the application verification
+		sleep 3
+		#Verify if the service is running
+		$PGREP -f $APP  > /dev/null
+		VERIFIER=$?
+		if [ $ZERO -eq $VERIFIER ]
+		then
+			echo -e "${RED}Failed${NC} to stop application"
+		else
+			echo -e "Application was ${GREEN}successfully${NC} stopped"
+		fi        
+    else
+        echo -e "The application is ${YELLOW}already${NC} stopped"
+    fi
+    echo
 }
 
 # Show the application status
 status() {
-  # The check_status function, again...
-  pid="$(check_status)"
-  
-  # If the PID was returned means the application is running
-  if [ "$pid" != 0 ] ; then
-    echo "Application is started with PID: ${pid}"
-  else
-    echo "Application is stopped"
-  fi
-
+	echo ""		
+    echo "Checking status of $APP ..."
+    #Verify if the application is running
+    $PGREP -f $APP > /dev/null
+    VERIFIER=$?
+    if [ $ZERO -eq $VERIFIER ]
+    then
+        echo -e "Application is ${GREEN}running${NC}"        
+    else
+        echo -e "Application is ${RED}stopped${NC}"
+    fi
+    echo
 }
 
 
 #verify that java is available
 check_java() {
-	JAVA_VERSION=`echo "$(java -version 2>&1 >/dev/null)" | grep 'java version' | awk '{print $3}'`
-
-	if [ $JAVA_VERSION ] ; then
-	     echo -e "${GREEN} Java version($JAVA_VERSION) is installed"
-	else
-		echo -e "${RED} Java is not availible"
-   		exit 1
-	fi
+        JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+        #echo $JAVA_VERSION
+        if [ $JAVA_VERSION ] ; then
+             echo -e "${GREEN} Java version($JAVA_VERSION) is installed!!"
+        else
+             echo -e "${RED} Java is not availible"
+             exit 1
+        fi
 }
 
 # Main logic, a simple case to call functions
